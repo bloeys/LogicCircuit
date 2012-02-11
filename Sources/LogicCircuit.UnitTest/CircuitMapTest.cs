4d40721f9ca2c66748cb756c3f440915f668a3e6
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2012-02-11 13:50:38 -0800

    Filing bug

---
+++ Sources/LogicCircuit.UnitTest/CircuitMapTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using LogicCircuit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicCircuit.UnitTest {
+	/// <summary>
+	///This is a test class for CircuitMapTest and is intended
+	///to contain all CircuitMapTest Unit Tests
+	///</summary>
+	[TestClass()]
+	public class CircuitMapTest {
+
+		#region Additional test attributes
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext { get; set; }
+
+		// 
+		//You can use the following additional attributes as you write your tests:
+		//
+		//Use ClassInitialize to run code before running the first test in the class
+		//[ClassInitialize()]
+		//public static void MyClassInitialize(TestContext testContext)
+		//{
+		//}
+		//
+		//Use ClassCleanup to run code after all tests in a class have run
+		//[ClassCleanup()]
+		//public static void MyClassCleanup()
+		//{
+		//}
+		//
+		//Use TestInitialize to run code before running each test
+		//[TestInitialize()]
+		//public void MyTestInitialize()
+		//{
+		//}
+		//
+		//Use TestCleanup to run code after each test has run
+		//[TestCleanup()]
+		//public void MyTestCleanup()
+		//{
+		//}
+		//
+		#endregion
+
+		private void CircuitMapPerfTest(string project, string initialCircuit, int maxCount, int maxSeconds) {
+			CircuitProject circuitProject = ProjectTester.Load(this.TestContext, project, initialCircuit);
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+			for(int i = 0; i < maxCount; i++) {
+				CircuitMap circuitMap = new CircuitMap(circuitProject.ProjectSet.Project.LogicalCircuit);
+				CircuitState circuitState = circuitMap.Apply(CircuitRunner.HistorySize);
+				Assert.IsNotNull(circuitState);
+				circuitMap.TurnOn();
+			}
+			stopwatch.Stop();
+			this.TestContext.WriteLine("{0} CircuitMap(s) created and applied in {1} - {2:N2} sec per each map", maxCount, stopwatch.Elapsed, stopwatch.Elapsed.TotalSeconds / maxCount);
+			Assert.IsTrue(stopwatch.Elapsed < new TimeSpan(0, 0, maxSeconds), "CircuitMap was created and applied successfully but too slow");
+		}
+
+		/// <summary>
+		///A test for Apply
+		///</summary>
+		[TestMethod()]
+		public void CircuitMapApplyPerfTest1() {
+			this.CircuitMapPerfTest(Properties.Resources.IntegerCalculator, "Test Computer", 50, 15);
+		}
+
+		/// <summary>
+		///A test for Apply
+		///</summary>
+		[TestMethod()]
+		public void CircuitMapApplyPerfTest2() {
+			this.CircuitMapPerfTest(Properties.Resources.ExternalCalculator, "Main", 10, 25);
+		}
+	}
+}
---
+++ Sources/LogicCircuit.UnitTest/ProjectTester.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using LogicCircuit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicCircuit.UnitTest {
+	/// <summary>
+	/// Helper class to load and prepare testing of project
+	/// </summary>
+	public class ProjectTester {
+		public CircuitProject CircuitProject { get; private set; }
+		public CircuitMap CircuitMap { get; private set; }
+		public CircuitState CircuitState { get; private set; }
+		public Project Project { get { return this.CircuitProject.ProjectSet.Project; } }
+		public FunctionConstant[] Input { get; private set; }
+		public FunctionProbe[] Output { get; private set; }
+
+		public ProjectTester(TestContext testContext, string projectText, string initialCircuit) {
+			this.CircuitProject = ProjectTester.Load(testContext, projectText, initialCircuit);
+
+			// Create map and state
+			this.CircuitMap = new CircuitMap(this.CircuitProject.ProjectSet.Project.LogicalCircuit);
+			this.CircuitState = this.CircuitMap.Apply(CircuitRunner.HistorySize);
+
+			// Init controlling points of the main circuit: constants and probes
+			List<CircuitSymbol> inputSymbol = new List<CircuitSymbol>();
+			foreach(CircuitSymbol symbol in this.Project.LogicalCircuit.CircuitSymbols()) {
+				if(symbol.Circuit is Constant) {
+					inputSymbol.Add(symbol);
+				}
+			}
+			CircuitSymbolComparer circuitSymbolComparer = new CircuitSymbolComparer(true);
+			inputSymbol.Sort(circuitSymbolComparer);
+			this.Input = inputSymbol.Select(s => (FunctionConstant)this.CircuitMap.Input(s)).ToArray();
+
+			List<CircuitSymbol> outputSymbol = new List<CircuitSymbol>();
+			foreach(CircuitSymbol symbol in this.Project.LogicalCircuit.CircuitSymbols()) {
+				Gate g = symbol.Circuit as Gate;
+				if(g != null && g.GateType == GateType.Probe) {
+					outputSymbol.Add(symbol);
+				}
+			}
+			outputSymbol.Sort(circuitSymbolComparer);
+			this.Output = outputSymbol.Select(s => this.CircuitMap.FunctionProbe(s)).ToArray();
+
+			this.CircuitMap.TurnOn();
+		}
+
+		public static CircuitProject Load(TestContext testContext, string projectText, string initialCircuit) {
+			// First save project text to test directory
+			string path = Path.Combine(testContext.TestRunDirectory, string.Format("{0}.{1}.{2}.xml", testContext.FullyQualifiedTestClassName, testContext.TestName, DateTime.UtcNow.Ticks));
+			File.WriteAllText(path, projectText, Encoding.UTF8);
+			// Load if from test directory
+			CircuitProject circuitProject = CircuitProject.Create(path);
+			File.Delete(path);
+			if(initialCircuit != null) {
+				LogicalCircuit circuit = circuitProject.LogicalCircuitSet.FindByName(initialCircuit);
+				Assert.IsNotNull(circuit, "initial circuit not found in the project");
+				if(circuitProject.ProjectSet.Project.LogicalCircuit != circuit) {
+					circuitProject.InOmitTransaction(() => circuitProject.ProjectSet.Project.LogicalCircuit = circuit);
+				}
+			}
+			ProjectTester.InitResources();
+			foreach(CircuitSymbol symbol in circuitProject.CircuitSymbolSet) {
+				symbol.GuaranteeGlyph();
+			}
+			return circuitProject;
+		}
+
+		public static void InitResources() {
+			// Init App resources so all the visual elements will be able to find them
+			if(App.CurrentApp == null) {
+				App app = new App();
+				if(Application.ResourceAssembly == null) {
+					Application.ResourceAssembly = typeof(App).Assembly;
+					app.InitializeComponent();
+				}
+			}
+		}
+	}
+}
